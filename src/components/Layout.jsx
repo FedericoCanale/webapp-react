6da12ab62429e58894d77ebf1d3f414b59e0455f
FedericoCanale <icocanale@gmail.com>
2025-12-11 16:00:32 +0100

    Add navbar with home link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,34 @@
+import { Link } from "react-router-dom";
+
 function Layout({ children }) {
     return (
         <div>
+            {/* navbar */}
             <header>
-                <h1>Webapp Film</h1>
+                <nav className="navbar navbar-dark bg-dark">
+                    <div className="container d-flex align-items-center">
+
+                        {/* Titolo */}
+                        <span className="navbar-brand mb-0 h1">
+                            WebApp Film
+                        </span>
+
+                        {/* Link Home */}
+                        <Link
+                            to="/"
+                            className="text-light"
+                            style={{ fontSize: "0.9rem" }}
+                        >
+                            Home
+                        </Link>
+                    </div>
+                </nav>
             </header>
 
-            <main>{children}</main>
+            {/* page */}
+            <main className="container mt-4">
+                {children}
+            </main>
         </div>
     );
 }
