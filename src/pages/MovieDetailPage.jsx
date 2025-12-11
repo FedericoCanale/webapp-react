d51ec15828f5c52afe01041e754086b7ede8df8c
FedericoCanale <icocanale@gmail.com>
2025-12-11 14:53:54 +0100

    Add falback message in case of a loading error

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -5,20 +5,32 @@ import axios from "axios";
 function MovieDetailPage() {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/movies/${id}`).then((res) => {
-            setMovie(res.data);
-        });
+        axios
+            .get(`http://localhost:3000/movies/${id}`)
+            .then((res) => {
+                setMovie(res.data);
+            })
+            .catch((err) => {
+                console.error("Errore nel caricamento del film:", err);
+                setError(true);
+            });
     }, [id]);
 
-    if (!movie) return <p>Caricamento...</p>;
+    if (error) {
+        return <p>Errore nel caricamento del film.</p>;
+    }
+
+    if (!movie) {
+        return <p>Caricamento...</p>;
+    }
 
     return (
         <div>
             <h2>{movie.title}</h2>
 
-            {/* Descrizione */}
             {movie.description && <p>{movie.description}</p>}
 
             <h3>Recensioni</h3>
@@ -26,14 +38,8 @@ function MovieDetailPage() {
             <ul>
                 {movie.reviews.map((review) => (
                     <li key={review.id}>
-                        {/* Autore */}
                         <strong>{review.name}</strong>
-
-                        {/* Voto */}
-                        {review.vote && ` (${review.vote}/5)`}
-
-                        {/* Testo */}
-                        : {review.text}
+                        {review.vote && ` (${review.vote}/5)`}: {review.text}
                     </li>
                 ))}
             </ul>
