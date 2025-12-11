ac242dda62cc3868e6e8252f877c62a427d9c1c9
FedericoCanale <icocanale@gmail.com>
2025-12-11 14:34:08 +0100

    Add details page to my webapp, basic and not 'clickable' from homepage

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function MovieDetailPage() {
     const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/movies/${id}`).then((res) => {
+            setMovie(res.data);
+        });
+    }, [id]);
+
+    if (!movie) return <p>Caricamento...</p>;
 
     return (
         <div>
-            <h2>Dettaglio film</h2>
-            <p>ID film: {id}</p>
+            <h2>{movie.title}</h2>
+
+            <h3>Recensioni</h3>
+
+            <ul>
+                {movie.reviews.map((review) => (
+                    <li key={review.id}>{review.text}</li>
+                ))}
+            </ul>
         </div>
     );
 }
