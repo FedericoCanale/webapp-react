9c58e27e8e04cec24cd567e7af3c530817024de3
FedericoCanale <icocanale@gmail.com>
2025-12-15 13:22:03 +0100

    Add state to ReviewForm inputs

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
+
 function ReviewForm() {
+    const [name, setName] = useState("");
+    const [vote, setVote] = useState(5);
+    const [text, setText] = useState("");
+
     return (
         <form>
             <h4>Aggiungi recensione</h4>
 
             <div>
                 <label>Nome</label><br />
-                <input />
+                <input value={name} onChange={(e) => setName(e.target.value)} />
             </div>
 
             <div>
                 <label>Voto</label><br />
-                <input type="number" min="1" max="5" />
+                <input
+                    type="number"
+                    min="1"
+                    max="5"
+                    value={vote}
+                    onChange={(e) => setVote(Number(e.target.value))}
+                />
             </div>
 
             <div>
                 <label>Recensione</label><br />
-                <textarea />
+                <textarea value={text} onChange={(e) => setText(e.target.value)} />
             </div>
 
             <button type="submit">Invia</button>
