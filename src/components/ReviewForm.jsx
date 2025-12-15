c96109e5b561a5c38b02625960acce4f6de6beab
FedericoCanale <icocanale@gmail.com>
2025-12-15 13:02:21 +0100

    Add input fields to ReviewForm

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,8 +1,25 @@
 function ReviewForm() {
     return (
-        <div>
+        <form>
             <h4>Aggiungi recensione</h4>
-        </div>
+
+            <div>
+                <label>Nome</label><br />
+                <input />
+            </div>
+
+            <div>
+                <label>Voto</label><br />
+                <input type="number" min="1" max="5" />
+            </div>
+
+            <div>
+                <label>Recensione</label><br />
+                <textarea />
+            </div>
+
+            <button type="submit">Invia</button>
+        </form>
     );
 }
 
